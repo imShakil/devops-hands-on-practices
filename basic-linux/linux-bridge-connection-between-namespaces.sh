7f734a9e0415d02586c16b5684f9f91ceff4ceb1
Mobarak Hosen Shakil <mh.ice.iu@gmail.com>
2024-02-03 23:38:50 +0600

    added linux bridge networking
    Signed-off-by: Mobarak Hosen Shakil <mh.ice.iu@gmail.com>

---
+++ basic-linux/linux-bridge-connection-between-namespaces.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# add two namespaces
+sudo ip netns add ns1
+sudo ip netns add ns2
+
+# Add pair of interfaces for namespaces
+sudo ip link add veth-ns1 type veth peer name veth-ns1-br
+sudo ip link add veth-ns2 type veth peer name veth-ns2-br
+
+# Connect virtual network interfaces at namespace end
+sudo ip link set veth-ns1 netns ns1
+sudo ip link set veth-ns2 netns ns2
+
+# Add a linux bridge network
+sudo ip link add dev br0 type bridge
+
+# Assign ip adddress for bridge network
+sudo ip link set dev br0 up
+sudo ip addr add 10.10.0.1/24 dev br0
+
+# Connect virutal network interfaces at birdge end
+sudo ip link set dev veth-ns1-br master br0
+sudo ip link set dev veth-ns2-br master br0
+
+# Turned up virtual interfaces at host
+sudo ip link set dev veth-ns1-br up
+sudo ip link set dev veth-ns2-br up 
+
+# Turned up virtual interfaces at namespace end
+sudo ip netns exec ns1 ip link set dev veth-ns1 
+sudo ip netns exec ns2 ip link set dev veth-ns2
+
+# Assign IP address and default route for virtual inerfaces at namespace end
+sudo ip netns exec ns1 ip addr add 10.10.0.10/24 dev veth-ns1
+sudo ip netns exec ns1 ip route add default via 10.10.0.1 dev veth-ns1 
+sudo ip netns exec ns2 ip addr add 10.10.0.20/24 dev veth-ns2
+sudo ip netns exec ns2 ip route add default via 10.10.0.1 dev veth-ns2
+
+# firewall
+
+# test connectivity
+ping -c 2 10.0.0.10
+ping -c 2 10.0.0.20
+sudo ip netns exec ns1 ping -c 2 10.10.0.20
+sudo ip netns exec ns2 ping -c 2 10.10.0.10
